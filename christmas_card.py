597772764d0071ef0d79b1bad2c18f477d4ddf09
Dylan Nelson <nlsdyl001@myuct.ac.za>
2020-12-30 18:29:57 +0200

    switch to community edition of manim; use custom font

--- christmas_card.py
+++ christmas_card.py
@@ -1,9 +1,16 @@
-from manimlib.imports import *
+from manim import *
+
+edwardian_script = TexTemplate(preamble=r"""
+\usepackage[no-math]{fontspec}
+\setmainfont[Mapping=tex-text]{Edwardian Script ITC}
+\usepackage[defaultmathsizes]{mathastext}""",
+tex_compiler="xelatex",
+output_format=".xdv")
 
 class ChristmasCard(Scene):
 
     def construct(self):
-        message = TextMobject("Merry Christmas")
+        message = Tex("Merry Christmas", tex_template=edwardian_script)
         self.play(Write(message), run_time=3)
         self.wait()
 
